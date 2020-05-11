c07007ec3bfc705085c986bf93b8bc788e4a7687
daniel-arnauer <daniel.arnauer@accenture.com>
2020-05-11 14:56:31 +0200

    Add Dropdown for permission type for projects

--- frontend/src/pages/Common/History/HistorySearch.js
+++ frontend/src/pages/Common/History/HistorySearch.js
@@ -25,6 +25,17 @@ const styles = {
 };
 
 const HistorySearch = ({ classes, fetchFirstHistoryEvents, users }) => {
+  const project_eventType = [
+    { id: "project_created", displayName: "Project created" },
+    { id: "project_updated", displayName: "Project updated" },
+    { id: "project_assigned", displayName: "Project assigned" },
+    { id: "project_closed", displayName: "Project closed" },
+    { id: "project_permission_granted", displayName: "Project permisision granted" },
+    { id: "project_permission_revoked", displayName: "Project permission revoked" },
+    { id: "project_projected_budget_updated", displayName: "Project budget updated" },
+    { id: "project_projected_budget_deleted", displayName: "Project budget closed" }
+  ];
+
   const [{ startAt, endAt, publisher, eventType }, mergeState, clearState] = useHistoryState();
 
   const onChange = e => {
@@ -41,7 +52,25 @@ const HistorySearch = ({ classes, fetchFirstHistoryEvents, users }) => {
     <div>
       <DatePicker className={classes.datepicker} label={"start"} name="startAt" value={startAt} onChange={onChange} />
       <DatePicker className={classes.datepicker} label={"end"} name="endAt" value={endAt} onChange={onChange} />
-      {renderPublisherSelection({ classes, publisher, onChange, users })}
+      {/* {renderPublisherSelection({ classes, publisher, onChange, users })}
+      {renderEventTypeSelection({ classes, eventType, onChange, project_eventType })} */}
+
+      {renderSelection({
+        classes,
+        value: publisher,
+        name: "publisher",
+        label: "Publisher",
+        onChange,
+        selectionList: users
+      })}
+      {renderSelection({
+        classes,
+        value: eventType,
+        name: "eventType",
+        label: "Permission type",
+        onChange,
+        selectionList: project_eventType
+      })}
       {/* // TODO: render permission selection since there are only certain types of events */}
       {/* permission search (User picker) */}
       <div className={classes.searchActions}>
@@ -52,7 +81,11 @@ const HistorySearch = ({ classes, fetchFirstHistoryEvents, users }) => {
           aria-label="search"
           data-test="reset"
           color="secondary"
-          onClick={() => fetchFirstHistoryEvents({ startAt, endAt, publisher, eventType })}
+          onClick={() => {
+            console.log(publisher);
+            console.log(eventType);
+            fetchFirstHistoryEvents({ startAt, endAt, publisher, eventType });
+          }}
         >
           {strings.common.search}
         </Button>
@@ -61,13 +94,13 @@ const HistorySearch = ({ classes, fetchFirstHistoryEvents, users }) => {
   );
 };
 
-const renderMenuItems = users => {
+const renderMenuItems = items => {
   let menuItems = [];
-  users.forEach(user => {
-    if (!user.isGroup) {
+  items.forEach(item => {
+    if (!item.isGroup) {
       menuItems.push(
-        <MenuItem key={`menu-item-${user.id}`} value={user.id}>
-          {user.displayName}
+        <MenuItem key={`menu-item-${item.id}`} value={item.id}>
+          {item.displayName}
         </MenuItem>
       );
     }
@@ -75,14 +108,40 @@ const renderMenuItems = users => {
   return menuItems;
 };
 
-const renderPublisherSelection = ({ classes, publisher, onChange, users }) => {
-  const userList = renderMenuItems(users);
+// const renderPublisherSelection = ({ classes, publisher, onChange, users }) => {
+//   const userList = renderMenuItems(users);
+
+//   return (
+//     <FormControl className={classes.formControl}>
+//       <InputLabel>Publisher</InputLabel>
+//       <Select value={publisher} name="publisher" onChange={onChange}>
+//         {userList}
+//       </Select>
+//     </FormControl>
+//   );
+// };
+
+// const renderEventTypeSelection = ({ classes, eventType, onChange, project_eventType }) => {
+//   const eventTypeList = renderMenuItems(project_eventType);
+
+//   return (
+//     <FormControl className={classes.formControl}>
+//       <InputLabel>Permission type</InputLabel>
+//       <Select value={eventType} name="eventType" onChange={onChange}>
+//         {eventTypeList}
+//       </Select>
+//     </FormControl>
+//   );
+// };
+
+const renderSelection = ({ classes, value, name, label, onChange, selectionList }) => {
+  const menuItemsList = renderMenuItems(selectionList);
 
   return (
     <FormControl className={classes.formControl}>
-      <InputLabel>Publisher</InputLabel>
-      <Select value={publisher} name="publisher" onChange={onChange}>
-        {userList}
+      <InputLabel>{label}</InputLabel>
+      <Select value={value} name={name} onChange={onChange}>
+        {menuItemsList}
       </Select>
     </FormControl>
   );
